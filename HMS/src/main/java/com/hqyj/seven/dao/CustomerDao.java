0f8ee788f1b06f4a3c659dbcf26ae20a469ebb3c
DESKTOP-RCLVOBJ\14353 <1435304038@qq.com>
2021-08-12 19:31:20 +0800

    在持久层增加按照id修改金额 的更新操作

--- HMS/src/main/java/com/hqyj/seven/dao/CustomerDao.java
+++ HMS/src/main/java/com/hqyj/seven/dao/CustomerDao.java
@@ -1,9 +1,11 @@
 package com.hqyj.seven.dao;
 
 import com.hqyj.seven.pojo.Customer;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface CustomerDao {
     List<Customer> queryAll();
+    int updataByCustomerIdToremainder(@Param("money") double money, @Param("id") int id);
 }
