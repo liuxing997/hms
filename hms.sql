03276a3d707bbc436d6c94c8efe941525b0ff472
nrd <1059938559@qq.com>
2021-08-12 09:52:13 +0800

    第五次修改数据库---------录入数据

--- hms.sql
+++ hms.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2021-08-11 18:28:38
+Date: 2021-08-12 09:50:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -32,13 +32,16 @@ CREATE TABLE `customer` (
 -- ----------------------------
 -- Records of customer
 -- ----------------------------
+INSERT INTO `customer` VALUES ('1', '王五', '5002331999112022222', '134123456', '134123456', '6000.00');
+INSERT INTO `customer` VALUES ('2', '狒兄', '5002331555115532122', '17347645215', '17347645215', '8000.00');
+INSERT INTO `customer` VALUES ('3', '至尊兄', '5002331999112055222', '18626262625', '18626262625', '5000000.00');
 
 -- ----------------------------
 -- Table structure for enter
 -- ----------------------------
 DROP TABLE IF EXISTS `enter`;
 CREATE TABLE `enter` (
-  `主键ID` int(11) NOT NULL COMMENT '主键ID',
+  `enter_id` int(11) NOT NULL COMMENT '主键ID',
   `house_id` int(11) DEFAULT NULL COMMENT '房间id',
   `customer_id` int(11) DEFAULT NULL COMMENT '客人id',
   `customer_info` varchar(255) DEFAULT NULL COMMENT '客人详细信息因为一间屋子可以住多个人',
@@ -51,12 +54,13 @@ CREATE TABLE `enter` (
   `user_id` int(11) DEFAULT NULL COMMENT '相关操作员id',
   `state` varchar(20) DEFAULT NULL COMMENT '状态,比如是否已结账',
   `price` float(10,2) DEFAULT NULL COMMENT '已谈好的房价',
-  PRIMARY KEY (`主键ID`)
+  PRIMARY KEY (`enter_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of enter
 -- ----------------------------
+INSERT INTO `enter` VALUES ('1', '1', '1', '1人', '2021-08-12 09:45:05', '2021-08-13 09:45:15', '2021-08-13 09:45:18', '3', '100', '1', '111111', '已结账', '100.00');
 
 -- ----------------------------
 -- Table structure for fee
@@ -78,6 +82,7 @@ CREATE TABLE `fee` (
 -- ----------------------------
 -- Records of fee
 -- ----------------------------
+INSERT INTO `fee` VALUES ('1', '1', '缴费', '3', '100', '1', '1人入住', '111111', '现金');
 
 -- ----------------------------
 -- Table structure for floor
@@ -92,25 +97,55 @@ CREATE TABLE `floor` (
 -- ----------------------------
 -- Records of floor
 -- ----------------------------
+INSERT INTO `floor` VALUES ('1', '一楼');
+INSERT INTO `floor` VALUES ('2', '二楼');
+INSERT INTO `floor` VALUES ('3', '三楼');
+INSERT INTO `floor` VALUES ('4', '四楼');
+INSERT INTO `floor` VALUES ('5', '五楼');
 
 -- ----------------------------
 -- Table structure for house
 -- ----------------------------
 DROP TABLE IF EXISTS `house`;
 CREATE TABLE `house` (
-  `house_id` int(11) NOT NULL COMMENT '主键ID',
+  `house_id` int(11) DEFAULT NULL COMMENT '主键ID',
   `name` varchar(50) DEFAULT NULL COMMENT '房间号',
   `amount` varchar(60) DEFAULT NULL COMMENT '可容纳人数的文字性说明',
   `price` float(10,2) DEFAULT NULL COMMENT '单价',
   `floor_id` int(2) DEFAULT NULL COMMENT '所在楼层',
   `state` varchar(60) DEFAULT NULL COMMENT '状态，在住、空闲、维修、已订 等等',
-  `customer_id` int(20) DEFAULT NULL COMMENT '当前在住的customer',
-  PRIMARY KEY (`house_id`)
+  `customer_id` int(20) DEFAULT NULL COMMENT '当前在住的customer'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of house
 -- ----------------------------
+INSERT INTO `house` VALUES ('1', '101', '2', '100.00', '1', '入住', '1');
+INSERT INTO `house` VALUES ('2', '102', '2', '100.00', '1', '空闲', null);
+INSERT INTO `house` VALUES ('3', '103', '2', '100.00', '1', '空闲', null);
+INSERT INTO `house` VALUES ('4', '104', '2', '100.00', '1', '空闲', null);
+INSERT INTO `house` VALUES ('5', '105', '2', '100.00', '1', '空闲', null);
+INSERT INTO `house` VALUES ('6', '201', '4', '150.00', '2', '空闲', null);
+INSERT INTO `house` VALUES ('7', '202', '4', '150.00', '2', '空闲', null);
+INSERT INTO `house` VALUES ('8', '203', '4', '150.00', '2', '空闲', null);
+INSERT INTO `house` VALUES ('9', '204', '4', '150.00', '2', '空闲', null);
+INSERT INTO `house` VALUES ('10', '205', '4', '150.00', '2', '空闲', null);
+INSERT INTO `house` VALUES ('11', '301', '1', '200.00', '3', '空闲', null);
+INSERT INTO `house` VALUES ('14', '304', '2', '100.00', '3', '空闲', null);
+INSERT INTO `house` VALUES ('14', '304', null, null, null, null, null);
+INSERT INTO `house` VALUES ('15', '305', '3', '240.00', '3', '空闲', null);
+INSERT INTO `house` VALUES ('14', '304', '2', '100.00', '3', '空闲', null);
+INSERT INTO `house` VALUES ('14', '304', '2', '100.00', '3', '空闲', null);
+INSERT INTO `house` VALUES ('14', '304', '2', '100.00', '3', '空闲', null);
+INSERT INTO `house` VALUES ('14', '304', '2', '100.00', '3', '空闲', null);
+INSERT INTO `house` VALUES ('14', '304', '2', '100.00', '3', '空闲', null);
+INSERT INTO `house` VALUES ('19', '404', '2', '240.00', '4', '空闲', null);
+INSERT INTO `house` VALUES ('19', '404', '2', '240.00', '4', '空闲', null);
+INSERT INTO `house` VALUES ('22', '506', '5', '666.00', '5', '空闲', null);
+INSERT INTO `house` VALUES ('22', '506', '5', '666.00', '5', '空闲', null);
+INSERT INTO `house` VALUES ('22', '506', '5', '666.00', '5', '空闲', null);
+INSERT INTO `house` VALUES ('22', '506', '5', '666.00', '5', '空闲', null);
+INSERT INTO `house` VALUES ('22', '506', '5', '666.00', '5', '空闲', null);
 
 -- ----------------------------
 -- Table structure for user
@@ -128,3 +163,5 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES ('111111', 'lisi', '123456', '5233', '活动');
+INSERT INTO `user` VALUES ('123456', 'zhangsan', '123456', '5244', '活动');
