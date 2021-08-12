52ad4220c178888b25aad16d58945b20d6dfc785
封锦 <1435304038@qq.com>
2021-08-12 15:30:27 +0800

    更改usedao的更新方法

--- HMS/src/main/java/com/hqyj/seven/dao/HouseDao.java
+++ HMS/src/main/java/com/hqyj/seven/dao/HouseDao.java
@@ -1,7 +1,10 @@
 package com.hqyj.seven.dao;
 
+import com.hqyj.seven.pojo.House;
+import com.hqyj.seven.pojo.User;
 import org.apache.ibatis.annotations.Param;
 
 public interface HouseDao {
          int updateByHouseName(@Param("customerId")int customerId,@Param("name") String name );
+         House queryByHousename(String name);
 }
