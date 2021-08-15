372d7e5214cd2a8d1a2646449b7fc33bfa2cd9ed
DESKTOP-RCLVOBJ\14353 <1435304038@qq.com>
2021-08-15 15:32:50 +0800

    在服务层添加查询所有住房信息的方法

--- HMS/src/main/java/com/hqyj/seven/service/EnterService.java
+++ HMS/src/main/java/com/hqyj/seven/service/EnterService.java
@@ -3,8 +3,12 @@ package com.hqyj.seven.service;
 import com.hqyj.seven.dao.EnterDao;
 import com.hqyj.seven.pojo.Enter;
 
+import java.util.List;
+
 public interface EnterService {
     int insertEnter(Enter enter);
     //安照房间号和住房时间查找住房id
     Enter queryByHouseIdAndTime(int CustomerId, String time);
+    //查询所有住房信息
+    public List<Enter> queryAll();
 }
