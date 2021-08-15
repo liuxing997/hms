334fdb84eb9af722a8de1a7772bf844486b39e98
DESKTOP-RCLVOBJ\14353 <1435304038@qq.com>
2021-08-15 15:31:10 +0800

    在持久层添加查询全部住房信息的功能

--- HMS/src/main/java/com/hqyj/seven/dao/EnterDao.java
+++ HMS/src/main/java/com/hqyj/seven/dao/EnterDao.java
@@ -3,7 +3,10 @@ package com.hqyj.seven.dao;
 import com.hqyj.seven.pojo.Enter;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface EnterDao {
     int inserintoEnter(Enter enter);
     Enter queryByHouseIdAndTime(@Param("HouseId") int HouseId, @Param("time") String time);
+    List<Enter> queryAll();
 }
