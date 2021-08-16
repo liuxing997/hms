3b5a2ac740b575a58fc28c59d8c9b166cd4454de
nrd <1059938559@qq.com>
2021-08-16 20:54:08 +0800

    上传数库

--- hms.sql
+++ hms.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2021-08-12 18:40:49
+Date: 2021-08-16 20:48:18
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -32,19 +32,21 @@ CREATE TABLE `customer` (
 -- ----------------------------
 -- Records of customer
 -- ----------------------------
-INSERT INTO `customer` VALUES ('1', '王五', '5002331999112022222', '134123456', '134123456', '6000.00');
-INSERT INTO `customer` VALUES ('2', '狒兄', '5002331555115532122', '17347645215', '17347645215', '8000.00');
-INSERT INTO `customer` VALUES ('3', '至尊兄', '5002331999112055222', '18626262625', '18626262625', '5000000.00');
+INSERT INTO `customer` VALUES ('1', '王五', '500233199911208888', '13522222222', '18555555555', '2720.00');
+INSERT INTO `customer` VALUES ('2', '狒兄', '5002331555115532122', '17347645215', '17347645215', '11850.00');
+INSERT INTO `customer` VALUES ('3', '至尊兄', '5002331999112055222', '18626262625', '18626262625', '4999200.00');
+INSERT INTO `customer` VALUES ('4', '钱六', '505201198906020103', '18795471230', '18795471230', '11680.00');
+INSERT INTO `customer` VALUES ('5', '韩寒', '500110192107212011', '13889444051', '13889444051', '100.00');
 
 -- ----------------------------
 -- Table structure for enter
 -- ----------------------------
 DROP TABLE IF EXISTS `enter`;
 CREATE TABLE `enter` (
-  `enter_id` int(11) NOT NULL COMMENT '主键ID',
+  `enter_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `house_id` int(11) DEFAULT NULL COMMENT '房间id',
   `customer_id` int(11) DEFAULT NULL COMMENT '客人id',
-  `customer_info` varchar(255) DEFAULT NULL COMMENT '客人详细信息因为一间屋子可以住多个人',
+  `customer_info` int(11) DEFAULT NULL COMMENT '客人详细信息因为一间屋子可以住多个人',
   `start_time` datetime DEFAULT NULL COMMENT '开始时间',
   `end_time_estimate` datetime DEFAULT NULL COMMENT '预估退房时间',
   `end_time_actual` datetime DEFAULT NULL COMMENT '实际退房时间',
@@ -54,19 +56,32 @@ CREATE TABLE `enter` (
   `state` varchar(20) DEFAULT NULL COMMENT '状态,比如是否已结账',
   `price` float(10,2) DEFAULT NULL COMMENT '已谈好的房价',
   PRIMARY KEY (`enter_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of enter
 -- ----------------------------
-INSERT INTO `enter` VALUES ('1', '1', '1', '1人', '2021-08-12 09:45:05', '2021-08-13 09:45:15', '2021-08-13 09:45:18', '100', '1', '111111', '已结账', '100.00');
+INSERT INTO `enter` VALUES ('1', '1', '1', '1', '2021-08-12 09:45:05', '2021-08-13 09:45:15', '2021-08-13 09:45:18', '100', '1', '111111', '已结账', '100.00');
+INSERT INTO `enter` VALUES ('38', '5', '2', '2', '2021-08-16 12:59:48', '2021-08-18 12:59:48', '2021-08-16 13:24:04', '200', '1', '222222', '已结账', '100.00');
+INSERT INTO `enter` VALUES ('39', '3', '1', '1', '2021-08-16 13:10:01', '2021-08-18 13:10:01', '2021-08-16 13:40:54', '1000', '1', '222222', '已结账', '500.00');
+INSERT INTO `enter` VALUES ('40', '4', '2', '1', '2021-08-16 13:43:05', '2021-08-18 13:43:05', '2021-08-16 13:48:15', '200', '1', '222222', '已结账', '100.00');
+INSERT INTO `enter` VALUES ('41', '6', '1', '2', '2021-08-16 13:45:43', '2021-08-18 13:45:43', '2021-08-16 14:24:59', '300', '1', '222222', '已结账', '150.00');
+INSERT INTO `enter` VALUES ('42', '1', '2', '1', '2021-08-16 13:47:58', '2021-08-19 13:47:58', '2021-08-16 13:48:06', '450', '1', '222222', '已结账', '150.00');
+INSERT INTO `enter` VALUES ('45', '3', '3', '2', '2021-08-16 14:17:55', '2021-08-18 14:17:55', '2021-08-16 14:24:46', '1000', '1', '222222', '已结账', '500.00');
+INSERT INTO `enter` VALUES ('46', '6', '1', '2', '2021-08-16 14:20:37', '2021-08-18 14:20:37', '2021-08-16 14:24:59', '300', '1', '222222', '已结账', '150.00');
+INSERT INTO `enter` VALUES ('47', '5', '3', '3', '2021-08-16 14:36:40', '2021-08-19 14:36:40', '2021-08-16 16:07:21', '300', '1', '222222', '已结账', '100.00');
+INSERT INTO `enter` VALUES ('48', '7', '1', '2', '2021-08-16 14:55:01', '2021-08-18 14:55:01', '2021-08-16 19:08:05', '0', '1', '222222', '待结账', '150.00');
+INSERT INTO `enter` VALUES ('49', '9', '1', '2', '2021-08-16 14:56:56', '2021-08-18 14:56:56', '2021-08-16 15:19:02', '0', '1', '222222', '待结账', '150.00');
+INSERT INTO `enter` VALUES ('50', '11', '3', '3', '2021-08-16 14:58:19', '2021-08-19 14:58:19', '2021-08-16 16:07:37', '600', '1', '222222', '已结账', '200.00');
+INSERT INTO `enter` VALUES ('51', '12', '4', '1', '2021-08-16 15:21:27', '2021-08-18 15:21:27', '2021-08-16 16:07:46', '200', '1', '222222', '已结账', '100.00');
+INSERT INTO `enter` VALUES ('52', '4', '4', '2', '2021-08-16 15:48:25', '2021-08-18 15:48:25', '2021-08-16 16:07:15', '200', '1', '555555', '已结账', '100.00');
 
 -- ----------------------------
 -- Table structure for fee
 -- ----------------------------
 DROP TABLE IF EXISTS `fee`;
 CREATE TABLE `fee` (
-  `fee_id` int(11) NOT NULL COMMENT '主键id',
+  `fee_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `enter_id` int(11) DEFAULT NULL COMMENT '登记表的id',
   `direct` varchar(20) DEFAULT NULL COMMENT '缴费还是退费',
   `coustomer_id` int(11) DEFAULT NULL COMMENT '客户号',
@@ -76,12 +91,31 @@ CREATE TABLE `fee` (
   `user_id` int(11) DEFAULT NULL COMMENT '操作员id',
   `fee_type` varchar(50) DEFAULT NULL COMMENT '现金、银行卡、微信、支付宝等等',
   PRIMARY KEY (`fee_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of fee
 -- ----------------------------
 INSERT INTO `fee` VALUES ('1', '1', '缴费', '3', '100', '1', '1人入住', '111111', '现金');
+INSERT INTO `fee` VALUES ('10', '33', '缴费', '1', '1500', '3', '1人已入住', '444444', '会员卡扣费');
+INSERT INTO `fee` VALUES ('11', '34', '缴费', '2', '200', '4', '1人已入住', '444444', '会员卡扣费');
+INSERT INTO `fee` VALUES ('12', '35', '缴费', '2', '1000', '3', '1人已入住', '444444', '会员卡扣费');
+INSERT INTO `fee` VALUES ('13', '36', '缴费', '1', '200', '5', '1人已入住', '444444', '会员卡扣费');
+INSERT INTO `fee` VALUES ('14', '37', '缴费', '2', '200', '4', '2人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('15', '38', '缴费', '2', '200', '5', '2人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('16', '39', '缴费', '1', '1000', '3', '1人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('17', '40', '缴费', '2', '200', '4', '1人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('18', '41', '缴费', '1', '300', '6', '2人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('19', '42', '缴费', '2', '450', '1', '1人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('20', '43', '缴费', '1', '200', '5', '2人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('21', '45', '缴费', '3', '1000', '3', '2人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('22', '46', '缴费', '1', '300', '6', '2人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('23', '47', '缴费', '3', '300', '5', '3人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('24', '48', '未缴费', '1', '300', '7', '2人已入住', '222222', '待缴费');
+INSERT INTO `fee` VALUES ('25', '49', '未缴费', '1', '300', '9', '2人已入住', '222222', '待缴费');
+INSERT INTO `fee` VALUES ('26', '50', '缴费', '3', '600', '11', '3人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('27', '51', '缴费', '4', '200', '12', '1人已入住', '222222', '会员卡扣费');
+INSERT INTO `fee` VALUES ('28', '52', '缴费', '4', '200', '4', '2人已入住', '555555', '会员卡扣费');
 
 -- ----------------------------
 -- Table structure for floor
@@ -119,18 +153,18 @@ CREATE TABLE `house` (
 -- ----------------------------
 -- Records of house
 -- ----------------------------
-INSERT INTO `house` VALUES ('1', '101', '2', '150.00', '1', '入住', '1');
-INSERT INTO `house` VALUES ('2', '102', '2', '500.00', '1', '空闲', null);
-INSERT INTO `house` VALUES ('3', '103', '2', '500.00', '1', '空闲', null);
-INSERT INTO `house` VALUES ('4', '104', '2', '100.00', '1', '空闲', null);
+INSERT INTO `house` VALUES ('1', '101', '2', '150.00', '1', '空闲', null);
+INSERT INTO `house` VALUES ('2', '102', '2', '500.00', '1', '维修', null);
+INSERT INTO `house` VALUES ('3', '103', '2', '500.00', '1', '已定', '5');
+INSERT INTO `house` VALUES ('4', '104', '2', '100.00', '1', '打扫', null);
 INSERT INTO `house` VALUES ('5', '105', '2', '100.00', '1', '空闲', null);
-INSERT INTO `house` VALUES ('6', '201', '4', '150.00', '2', '空闲', null);
-INSERT INTO `house` VALUES ('7', '202', '4', '150.00', '2', '空闲', null);
-INSERT INTO `house` VALUES ('8', '203', '4', '150.00', '2', '空闲', null);
-INSERT INTO `house` VALUES ('9', '204', '4', '150.00', '2', '空闲', null);
+INSERT INTO `house` VALUES ('6', '201', '4', '150.00', '2', '已定', '3');
+INSERT INTO `house` VALUES ('7', '202', '4', '150.00', '2', '打扫', null);
+INSERT INTO `house` VALUES ('8', '203', '4', '150.00', '2', '维修', null);
+INSERT INTO `house` VALUES ('9', '204', '4', '150.00', '2', '打扫', null);
 INSERT INTO `house` VALUES ('10', '205', '4', '150.00', '2', '空闲', null);
 INSERT INTO `house` VALUES ('11', '301', '1', '200.00', '3', '空闲', null);
-INSERT INTO `house` VALUES ('12', '302', '2', '100.00', '3', '空闲', null);
+INSERT INTO `house` VALUES ('12', '302', '2', '100.00', '3', '打扫', null);
 INSERT INTO `house` VALUES ('13', '303', '2', '100.00', '3', '空闲', null);
 INSERT INTO `house` VALUES ('14', '304', '3', '240.00', '3', '空闲', null);
 INSERT INTO `house` VALUES ('15', '305', '2', '100.00', '3', '空闲', null);
@@ -142,9 +176,8 @@ INSERT INTO `house` VALUES ('20', '405', '2', '240.00', '4', '空闲', null);
 INSERT INTO `house` VALUES ('21', '501', '2', '240.00', '4', '空闲', null);
 INSERT INTO `house` VALUES ('22', '502', '5', '666.00', '5', '空闲', null);
 INSERT INTO `house` VALUES ('23', '503', '5', '666.00', '5', '空闲', null);
-INSERT INTO `house` VALUES ('24', '504', '5', '666.00', '5', '空闲', null);
-INSERT INTO `house` VALUES ('25', '505', '5', '666.00', '5', '空闲', null);
-INSERT INTO `house` VALUES ('26', '601', '1', '100000.00', '6', '空闲', null);
+INSERT INTO `house` VALUES ('24', '504', '2', '500000.00', '5', '已定', '3');
+INSERT INTO `house` VALUES ('25', '505', '4', '5000.00', '5', '空闲', null);
 
 -- ----------------------------
 -- Table structure for user
@@ -162,5 +195,11 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('111111', 'lisi', '123456', '5233', '活动');
-INSERT INTO `user` VALUES ('123456', 'zhangsan', '123456', '5244', '活动');
+INSERT INTO `user` VALUES ('111111', 'liuxing', 'c64185298e81f47420f7b3094f07258c', '18888888888', '活动');
+INSERT INTO `user` VALUES ('222222', 'nrd', '12ce18a8d6c0b9978cfe0a843ddfbc9d', '13667777777', '活动');
+INSERT INTO `user` VALUES ('232424', 'liuxing1', '62ff374856390347d73118985c06abb5', '1141412414', '活动');
+INSERT INTO `user` VALUES ('333333', 'zzy', '4b452940e12efbce7fb939fb80d816fc', '19823234224', '活动');
+INSERT INTO `user` VALUES ('444444', 'fj', 'a00fbd7d2fb7e08f8c48cb2887548a15', '1366999999999', '禁用');
+INSERT INTO `user` VALUES ('555555', 'hjh', 'd79e2aec3a0646ab6464c96ca0dcc0d9', '12411415985', '活动');
+INSERT INTO `user` VALUES ('888888', 'zhangsan', '464417c490a2935ed383be70975dfc7d', '14141531515', '其他');
+INSERT INTO `user` VALUES ('999999', 'lisi', '72fc30577f51cea8641b745bbc292c6e', '12397129721', '禁用');
