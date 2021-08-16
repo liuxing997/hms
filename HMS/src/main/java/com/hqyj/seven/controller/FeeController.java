73a27a385b43f4c6f2d9a3657738ee39347dab38
刘兴 <liuxing997@foxmail.com>
2021-08-16 18:35:46 +0800

    格式化代码

--- HMS/src/main/java/com/hqyj/seven/controller/FeeController.java
+++ HMS/src/main/java/com/hqyj/seven/controller/FeeController.java
@@ -1,6 +1,7 @@
 package com.hqyj.seven.controller;
-import com.hqyj.seven.pojo.*;
-import com.hqyj.seven.service.EnterService;
+
+import com.hqyj.seven.pojo.Fee;
+import com.hqyj.seven.pojo.PageData;
 import com.hqyj.seven.service.FeeService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.session.Session;
@@ -10,10 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+
 @Controller
 @RequestMapping("/fee")
 public class FeeController {
@@ -23,128 +23,132 @@ public class FeeController {
     public void setFeeService(FeeService feeService) {
         this.feeService = feeService;
     }
+
     //查询所有已缴费信息
     @RequestMapping("/queryByDirectPay")
     @ResponseBody
-    public Map<String,Object> queryByDirectPay(@RequestParam("page") Integer pageNumber, @RequestParam("limit")Integer pageSize) {
+    public Map<String, Object> queryByDirectPay(@RequestParam("page") Integer pageNumber, @RequestParam("limit") Integer pageSize) {
         int number;
         int size;
-        if (pageNumber == null){
+        if (pageNumber == null) {
             number = 1;
-        }else {
+        } else {
             number = pageNumber;
         }
-        if (pageSize == null){
+        if (pageSize == null) {
             size = 10;
-        }else {
+        } else {
             size = pageSize;
         }
         //添加分页功能
-        PageData<Fee> feeList =feeService.queryByDirectPay(number,size);
-        Map<String,Object> fee =  new HashMap<>();
-        if (feeList == null){
-            fee.put("code",-1);
-            fee.put("msg","没有客房信息");
-        }else {
-           fee.put("code", 0);
+        PageData<Fee> feeList = feeService.queryByDirectPay(number, size);
+        Map<String, Object> fee = new HashMap<>();
+        if (feeList == null) {
+            fee.put("code", -1);
+            fee.put("msg", "没有客房信息");
+        } else {
+            fee.put("code", 0);
             fee.put("data", feeList);
-            fee.put("msg","获取数据成功！");
+            fee.put("msg", "获取数据成功！");
         }
-        Session session= SecurityUtils.getSubject().getSession();
-        session.setAttribute("feelist",feeList);
+        Session session = SecurityUtils.getSubject().getSession();
+        session.setAttribute("feelist", feeList);
         return fee;
     }
+
     //查询所有未缴费信息
     @RequestMapping("/queryByDirectNoPay")
     @ResponseBody
-    public Map<String,Object> queryByDirectNoPay(@RequestParam("page") Integer pageNumber, @RequestParam("limit")Integer pageSize) {
+    public Map<String, Object> queryByDirectNoPay(@RequestParam("page") Integer pageNumber, @RequestParam("limit") Integer pageSize) {
         int number;
         int size;
-        if (pageNumber == null){
+        if (pageNumber == null) {
             number = 1;
-        }else {
+        } else {
             number = pageNumber;
         }
-        if (pageSize == null){
+        if (pageSize == null) {
             size = 10;
-        }else {
+        } else {
             size = pageSize;
         }
         //添加分页功能
-        PageData<Fee> feeList =feeService.queryByDirectNoPay(number,size);
-        Map<String,Object> fee =  new HashMap<>();
-        if (feeList == null){
-            fee.put("code",-1);
-            fee.put("msg","没有客房信息");
-        }else {
+        PageData<Fee> feeList = feeService.queryByDirectNoPay(number, size);
+        Map<String, Object> fee = new HashMap<>();
+        if (feeList == null) {
+            fee.put("code", -1);
+            fee.put("msg", "没有客房信息");
+        } else {
             fee.put("code", 0);
-           fee.put("data", feeList);
-            fee.put("msg","获取数据成功！");
+            fee.put("data", feeList);
+            fee.put("msg", "获取数据成功！");
         }
-        Session session= SecurityUtils.getSubject().getSession();
-        session.setAttribute("feelist",feeList);
+        Session session = SecurityUtils.getSubject().getSession();
+        session.setAttribute("feelist", feeList);
         return fee;
     }
+
     //缴费查询
     @RequestMapping("/queryOneByIdPay")
     @ResponseBody
-    public  Map<String,Object> queryOneByIdPay(@RequestParam("page") Integer pageNumber, @RequestParam("limit")Integer pageSize,int id){
+    public Map<String, Object> queryOneByIdPay(@RequestParam("page") Integer pageNumber, @RequestParam("limit") Integer pageSize, int id) {
         int number;
         int size;
-        if (pageNumber == null){
+        if (pageNumber == null) {
             number = 1;
-        }else {
+        } else {
             number = pageNumber;
         }
-        if (pageSize == null){
+        if (pageSize == null) {
             size = 10;
-        }else {
+        } else {
             size = pageSize;
         }
         //添加分页功能
-        PageData<Fee> feeList =feeService.queryAllId(number,size,id,"缴费");
-        Map<String,Object> fee =  new HashMap<>();
-        if (feeList == null){
-            fee.put("code",-1);
-            fee.put("msg","没有缴费信息");
-        }else {
+        PageData<Fee> feeList = feeService.queryAllId(number, size, id, "缴费");
+        Map<String, Object> fee = new HashMap<>();
+        if (feeList == null) {
+            fee.put("code", -1);
+            fee.put("msg", "没有缴费信息");
+        } else {
             fee.put("code", 0);
             fee.put("data", feeList);
-            fee.put("msg","获取数据成功！");
+            fee.put("msg", "获取数据成功！");
         }
-        Session session= SecurityUtils.getSubject().getSession();
-        session.setAttribute("feelist",feeList);
+        Session session = SecurityUtils.getSubject().getSession();
+        session.setAttribute("feelist", feeList);
         return fee;
     }
+
     //未缴费查询
     @RequestMapping("/queryOneByIdNoPay")
     @ResponseBody
-    public  Map<String,Object> queryOneByIdNoPay(@RequestParam("page") Integer pageNumber, @RequestParam("limit")Integer pageSize,int id){
+    public Map<String, Object> queryOneByIdNoPay(@RequestParam("page") Integer pageNumber, @RequestParam("limit") Integer pageSize, int id) {
         int number;
         int size;
-        if (pageNumber == null){
+        if (pageNumber == null) {
             number = 1;
-        }else {
+        } else {
             number = pageNumber;
         }
-        if (pageSize == null){
+        if (pageSize == null) {
             size = 10;
-        }else {
+        } else {
             size = pageSize;
         }
         //添加分页功能
-        PageData<Fee> feeList =feeService.queryAllId(number,size,id,"未缴费");
-        Map<String,Object> fee =  new HashMap<>();
-        if (feeList == null){
-            fee.put("code",-1);
-            fee.put("msg","没有缴费信息");
-        }else {
+        PageData<Fee> feeList = feeService.queryAllId(number, size, id, "未缴费");
+        Map<String, Object> fee = new HashMap<>();
+        if (feeList == null) {
+            fee.put("code", -1);
+            fee.put("msg", "没有缴费信息");
+        } else {
             fee.put("code", 0);
             fee.put("data", feeList);
-            fee.put("msg","获取数据成功！");
+            fee.put("msg", "获取数据成功！");
         }
-        Session session= SecurityUtils.getSubject().getSession();
-        session.setAttribute("feelist",feeList);
+        Session session = SecurityUtils.getSubject().getSession();
+        session.setAttribute("feelist", feeList);
         return fee;
     }
 }
