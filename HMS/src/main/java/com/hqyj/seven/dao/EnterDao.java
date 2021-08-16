15ee8b44745febedfd01a95a24dcb97c39c8df99
fengjin <1435304038@qq.com>
2021-08-16 13:28:09 +0800

    在enter添加按住房id 和顾客id查找和修改的功能

--- HMS/src/main/java/com/hqyj/seven/dao/EnterDao.java
+++ HMS/src/main/java/com/hqyj/seven/dao/EnterDao.java
@@ -3,6 +3,7 @@ package com.hqyj.seven.dao;
 import com.hqyj.seven.pojo.Enter;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface EnterDao {
@@ -12,4 +13,6 @@ public interface EnterDao {
     List<Enter> queryAll();
     //删除记录
     int deleteEnter(int enter_id);
+    int  updateByHouseIdAndCustomerId(@Param("time") Date time, @Param("houseId") int houseId, @Param("customerId") int customerId);
+    Enter selectByHouseIdAndCustomerId(@Param("houseId") int houseId,@Param("customerId") int customerId);
 }
