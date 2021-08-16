6909cef492b3294932a269b977f0061f5bdcae20
fengjin <1435304038@qq.com>
2021-08-16 16:46:13 +0800

    在服务层实现 分别查询缴费和未缴费的全部信息

--- HMS/src/main/java/com/hqyj/seven/service/FeeService.java
+++ HMS/src/main/java/com/hqyj/seven/service/FeeService.java
@@ -2,10 +2,15 @@ package com.hqyj.seven.service;
 
 import com.hqyj.seven.pojo.Enter;
 import com.hqyj.seven.pojo.Fee;
+import com.hqyj.seven.pojo.House;
+import com.hqyj.seven.pojo.PageData;
 
 public interface FeeService {
     //插入数据
     int insertFee(Fee fee);
-    //安照房间号和住房时间查找住房id
+    //查询已经缴费信息表
+    public PageData<Fee> queryByDirectPay(int pageNumber, int pageSize);
+    //查询未缴费信息表
+    public PageData<Fee> queryByDirectNoPay(int pageNumber, int pageSize);
 
 }
